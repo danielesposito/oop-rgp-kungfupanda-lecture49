70cf5e8182dd9e97513dc836810eb04af0654b41
Daniel Esposito <danielesposito@macbkk.com>
2016-03-26 01:13:25 +0700

    Add player class files

---
+++ rgp-ooc-game-assigment/PlayerA.swift
@@ -0,0 +1,9 @@
+//
+//  PlayerA.swift
+//  rgp-ooc-game-assigment
+//
+//  Created by Daniel Esposito on 3/26/16.
+//  Copyright © 2016 Daniel Esposito. All rights reserved.
+//
+
+import Foundation
