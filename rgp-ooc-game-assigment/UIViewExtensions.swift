82b4c33820daaeac7746b1186801c6ddb08b3109
Daniel Esposito <danielesposito@macbkk.com>
2016-03-29 20:47:13 +0700

    Added small animation to bonus label
    Thanks to: https://www.andrewcbancroft.com/2014/07/27/fade-in-out-animations-as-class-extensions-with-swift/

---
+++ rgp-ooc-game-assigment/UIViewExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewExtensions.swift
+//  rgp-ooc-game-assigment
+//
+//  Created by Daniel Esposito on 3/29/16.
+//  Copyright © 2016 Daniel Esposito. All rights reserved.
+//
+
+import Foundation
